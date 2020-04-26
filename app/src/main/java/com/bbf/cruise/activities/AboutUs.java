f34cf1366e24e68650a81da1ef99391de9b1efb8
Vukasin <jovic.vukasin@gmail.com>
2020-04-26 16:41:36 +0200

    dunno

---
+++ app/src/main/java/com/bbf/cruise/activities/AboutUs.java
@@ -0,0 +1,16 @@
+package com.bbf.cruise.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bbf.cruise.R;
+
+public class AboutUs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+}
