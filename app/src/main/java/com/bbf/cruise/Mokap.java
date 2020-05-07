490ceeae578fb7e2e27b7d809e1ca19708ca4645
Vukasin <jovic.vukasin@gmail.com>
2020-05-07 14:26:15 +0200

    makeup

--- app/src/main/java/com/bbf/cruise/Mokap.java
+++ app/src/main/java/com/bbf/cruise/Mokap.java
@@ -14,9 +14,23 @@ public class Mokap {
         RideHistoryItem item2 = new RideHistoryItem("10.10.2020. 15:30", "10.10.2020. 15:58", 10, 500, 1000);
         RideHistoryItem item3 = new RideHistoryItem("10.10.2020. 15:30", "10.10.2020. 15:58", 10, 500, 1000);
 
+        RideHistoryItem item4 = new RideHistoryItem("10.10.2020. 15:30", "10.10.2020. 15:58", 10, 500, 1000);
+        RideHistoryItem item5 = new RideHistoryItem("10.10.2020. 15:30", "10.10.2020. 15:58", 10, 500, 1000);
+        RideHistoryItem item6 = new RideHistoryItem("10.10.2020. 15:30", "10.10.2020. 15:58", 10, 500, 1000);
+        RideHistoryItem item7 = new RideHistoryItem("10.10.2020. 15:30", "10.10.2020. 15:58", 10, 500, 1000);
+
+
+
+
         rideHistoryItems.add(item1);
         rideHistoryItems.add(item2);
         rideHistoryItems.add(item3);
+
+        rideHistoryItems.add(item3);
+        rideHistoryItems.add(item4);
+        rideHistoryItems.add(item5);
+        rideHistoryItems.add(item6);
+        rideHistoryItems.add(item7);
 
         return rideHistoryItems;
     }
