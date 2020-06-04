561f5458828ef40a442c1f5e3f3b83194a5f4166
Vladimir Cvetanovic <cvetanovic9696@gmail.com>
2020-06-04 12:26:45 +0200

    Prikaz kola na mapi koja upadaju u radijus

--- app/src/main/java/com/bbf/cruise/fragments/MapFragment.java
+++ app/src/main/java/com/bbf/cruise/fragments/MapFragment.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
@@ -62,6 +63,8 @@ import java.util.List;
 
 import model.Car;
 
+import static android.content.Context.MODE_PRIVATE;
+
 public class MapFragment extends Fragment implements LocationListener, OnMapReadyCallback {
 
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
@@ -76,6 +79,9 @@ public class MapFragment extends Fragment implements LocationListener, OnMapRead
     private List<Car> cars = new ArrayList<>();
     private Button centreButton;
     private Dialog mDialog;
+
+    public final static double AVERAGE_RADIUS_OF_EARTH = 6371;
+
 
     public static MapFragment newInstance() {
 
@@ -446,8 +452,14 @@ public class MapFragment extends Fragment implements LocationListener, OnMapRead
     }
 
     private void positionCarMarkers(){
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("sharedPrefs", MODE_PRIVATE);
+        int radius = sharedPreferences.getInt("radius", 30);
         for(Car car: cars){
-            addCarMarker(car);
+            int distance = calculateDistance(home.getPosition().latitude, home.getPosition().longitude,
+                    car.getLocation().getLatitude(), car.getLocation().getLongitude());
+            if(distance <= radius){
+                addCarMarker(car);
+            }
         }
     }
 
@@ -474,5 +486,22 @@ public class MapFragment extends Fragment implements LocationListener, OnMapRead
         super.onPause();
 
         locationManager.removeUpdates(this);
+    }
+
+    private int calculateDistance(double userLat, double userLng, double venueLat, double venueLng) {
+
+        double latDistance = Math.toRadians(userLat - venueLat);
+        double lngDistance = Math.toRadians(userLng - venueLng);
+
+        double a = (Math.sin(latDistance / 2) * Math.sin(latDistance / 2)) +
+                (Math.cos(Math.toRadians(userLat))) *
+                        (Math.cos(Math.toRadians(venueLat))) *
+                        (Math.sin(lngDistance / 2)) *
+                        (Math.sin(lngDistance / 2));
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return (int) (Math.round(AVERAGE_RADIUS_OF_EARTH * c));
+
     }
 }
